3eaac08feee069e65fe2ff0e84114a2046b3c4a5
Arindam Majumder <arindammajumder2020@gmail.com>
2024-06-26 22:20:51 +0530

    fix

--- index.js
+++ index.js
@@ -71,7 +71,8 @@ const checkAuth = (req, res, next) => {
 };
 
 app.get('/', (req, res) => {
-  res.render('index', { loggedIn: !!accessToken });
+  // res.render('index', { loggedIn: !!accessToken });
+  res.send('Hello World');
 });
 
 app.get('/login', (req, res) => {
