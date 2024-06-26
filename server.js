34d625750753e6fc791266a41542fe4650ebda86
Arindam Majumder <arindammajumder2020@gmail.com>
2024-06-26 21:05:36 +0530

    minor changes

--- server.js
+++ server.js
@@ -44,7 +44,6 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 app.use(async (req, res, next) => {
   try {
     const decision = await aj.protect(req);
